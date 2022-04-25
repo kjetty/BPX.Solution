1c81cc49468da46021eb8d9b6435ac06c401061a
Ketan Jetty <kjetty@yahoo.com>
2022-04-25 15:05:18 -0400

    added new generated permit constants file

--- BPX.Website/CustomCode/Authorize/PermitConstants.cs
+++ BPX.Website/CustomCode/Authorize/PermitConstants.cs
@@ -1,80 +1,80 @@
-//This file is auto generated on 2/24/2022 2:18:59 PM
+//This file is auto generated on 4/25/2022 3:03:48 PM
 namespace BPX.Website.CustomCode.Authorize
-{ 
+{
 	public static class Permits
-	{ 
+	{
 		public static class Developer
-		{ 
+		{
 			public static class ErrorLog
-			{ 
-				public const int DownloadLog = 3; 			 //Developer.ErrorLog.DownloadLog
-				public const int Index = 2; 			 //Developer.ErrorLog.Index
-			} 
+			{
+				public const int DownloadLog = 2;            //Developer.ErrorLog.DownloadLog
+				public const int Index = 1;              //Developer.ErrorLog.Index
+			}
 
 			public static class PermitsGenerator
-			{ 
-				public const int Index = 1; 			 //Developer.Home.Index
-			} 
+			{
+				public const int Index = 3;              //Developer.Home.Index
+			}
 
-		} 
+		}
 
 		public static class Identity
-		{ 
+		{
 			public static class Menu
-			{ 
-				public const int Create = 32; 			 //Identity.Menu.Create
-				public const int Delete = 35; 			 //Identity.Menu.Delete
-				public const int Filter = 37; 			 //Identity.Menu.Filter
-				public const int List = 36; 			 //Identity.Menu.List
-				public const int ListDeleted = 38; 			 //Identity.Menu.ListDeleted
-				public const int MenuPermits = 40; 			 //Identity.Menu.MenuPermits
-				public const int Read = 33; 			 //Identity.Menu.Read
-				public const int TreePath = 41; 			 //Identity.Menu.TreePath
-				public const int Undelete = 39; 			 //Identity.Menu.Undelete
-				public const int Update = 34; 			 //Identity.Menu.Update
-			} 
+			{
+				public const int Create = 4;             //Identity.Menu.Create
+				public const int Delete = 7;             //Identity.Menu.Delete
+				public const int Filter = 9;             //Identity.Menu.Filter
+				public const int List = 8;           //Identity.Menu.List
+				public const int ListDeleted = 10;           //Identity.Menu.ListDeleted
+				public const int MenuPermits = 12;           //Identity.Menu.MenuPermits
+				public const int Read = 5;           //Identity.Menu.Read
+				public const int TreePath = 13;              //Identity.Menu.TreePath
+				public const int Undelete = 11;              //Identity.Menu.Undelete
+				public const int Update = 6;             //Identity.Menu.Update
+			}
 
 			public static class Permit
-			{ 
-				public const int Create = 23; 			 //Identity.Permit.Create
-				public const int Delete = 26; 			 //Identity.Permit.Delete
-				public const int Filter = 28; 			 //Identity.Permit.Filter
-				public const int List = 27; 			 //Identity.Permit.List
-				public const int ListDeleted = 29; 			 //Identity.Permit.ListDeleted
-				public const int PermitRolesMenus = 31; 			 //Identity.Permit.PermitRolesMenus
-				public const int Read = 24; 			 //Identity.Permit.Read
-				public const int Undelete = 30; 			 //Identity.Permit.Undelete
-				public const int Update = 25; 			 //Identity.Permit.Update
-			} 
+			{
+				public const int Create = 14;            //Identity.Permit.Create
+				public const int Delete = 17;            //Identity.Permit.Delete
+				public const int Filter = 19;            //Identity.Permit.Filter
+				public const int List = 18;              //Identity.Permit.List
+				public const int ListDeleted = 20;           //Identity.Permit.ListDeleted
+				public const int PermitRolesMenus = 22;              //Identity.Permit.PermitRolesMenus
+				public const int Read = 15;              //Identity.Permit.Read
+				public const int Undelete = 21;              //Identity.Permit.Undelete
+				public const int Update = 16;            //Identity.Permit.Update
+			}
 
 			public static class Role
-			{ 
-				public const int Create = 14; 			 //Identity.Role.Create
-				public const int Delete = 17; 			 //Identity.Role.Delete
-				public const int Filter = 19; 			 //Identity.Role.Filter
-				public const int List = 18; 			 //Identity.Role.List
-				public const int ListDeleted = 20; 			 //Identity.Role.ListDeleted
-				public const int Read = 15; 			 //Identity.Role.Read
-				public const int RolePermits = 22; 			 //Identity.Role.RolePermits
-				public const int Undelete = 21; 			 //Identity.Role.Undelete
-				public const int Update = 16; 			 //Identity.Role.Update
-			} 
+			{
+				public const int Create = 23;            //Identity.Role.Create
+				public const int Delete = 26;            //Identity.Role.Delete
+				public const int Filter = 28;            //Identity.Role.Filter
+				public const int List = 27;              //Identity.Role.List
+				public const int ListDeleted = 29;           //Identity.Role.ListDeleted
+				public const int Read = 24;              //Identity.Role.Read
+				public const int RolePermits = 31;           //Identity.Role.RolePermits
+				public const int Undelete = 30;              //Identity.Role.Undelete
+				public const int Update = 25;            //Identity.Role.Update
+			}
 
 			public static class User
-			{ 
-				public const int ChangePassword = 12; 			 //Identity.User.ChangePassword
-				public const int Create = 4; 			 //Identity.User.Create
-				public const int Delete = 7; 			 //Identity.User.Delete
-				public const int Filter = 9; 			 //Identity.User.Filter
-				public const int List = 8; 			 //Identity.User.List
-				public const int ListDeleted = 10; 			 //Identity.User.ListDeleted
-				public const int Read = 5; 			 //Identity.User.Read
-				public const int Undelete = 11; 			 //Identity.User.Undelete
-				public const int Update = 6; 			 //Identity.User.Update
-				public const int UserRoles = 13; 			 //Identity.User.UserRoles
-			} 
+			{
+				public const int ChangePassword = 40;            //Identity.User.ChangePassword
+				public const int Create = 32;            //Identity.User.Create
+				public const int Delete = 35;            //Identity.User.Delete
+				public const int Filter = 37;            //Identity.User.Filter
+				public const int List = 36;              //Identity.User.List
+				public const int ListDeleted = 38;           //Identity.User.ListDeleted
+				public const int Read = 33;              //Identity.User.Read
+				public const int Undelete = 39;              //Identity.User.Undelete
+				public const int Update = 34;            //Identity.User.Update
+				public const int UserRoles = 41;             //Identity.User.UserRoles
+			}
 
-		} 
+		}
 
-	} 
-} 
+	}
+}

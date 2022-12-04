1f97bcf7b15f514c3ba10c98fa4ee1f1414c9c81
Ketan Jetty <kjetty@yahoo.com>
2022-12-04 18:57:03 -0500

    nlog proper code

--- BPX.Website/Program.cs
+++ BPX.Website/Program.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using NLog.Extensions.Logging;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
 using System;
 
 namespace BPX.Website
@@ -10,26 +11,41 @@ namespace BPX.Website
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-            //.ConfigureAppConfiguration((context, configuration) =>
-            //{
-            //    configuration.Sources.Clear();
-            //    var bpxEnvironmentName = Environment.GetEnvironmentVariable("DOTNETCORE_ENVIRONMENT");
-            //    configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-            //    configuration.AddJsonFile("appsettings.{bpxEnvironmentName}.json", optional: true, reloadOnChange: true);
-            //    configuration.AddCommandLine(args);
-            //})
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
             })
-            .ConfigureLogging((hostingContext, logging) =>
+            .ConfigureLogging(logging =>
             {
-                logging.AddNLog(hostingContext.Configuration.GetSection("Logging"));                
-            });
+                logging.ClearProviders();
+                //logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+            })
+            .UseNLog();
+            //.ConfigureLogging((hostingContext, logging) =>
+            //{
+            //    logging.AddNLog(hostingContext.Configuration.GetSection("Logging"));
+            //});
     }
 }
